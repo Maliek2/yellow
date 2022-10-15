dde7c85b5984da7ce7671f427b6dc928cfe06a5f
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-10-15 12:39:40 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
+    title: 'News Blog',
     siteUrl: 'https://yellowcake.netlify.com'
   },
   plugins: [
@@ -26,7 +26,7 @@ module.exports = {
         theme_color: '#00C2BD',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
+        display: 'fullscreen,
         icon: `${__dirname}/static/images/logo.svg` // This path is relative to the root of the site.
       }
     },
